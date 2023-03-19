8e6661241af5fb827656a7989e54c49e4e082906
Mawit Gad <mawitgaddev@gmail.com>
2023-03-19 16:09:39 +0000

    Fix: make json parse get string

--- bookmarkStore.ts
+++ bookmarkStore.ts
@@ -9,22 +9,26 @@ export default class BookmarkStore {
 
   addBookmark(bookmark: BookmarkInterface) {
     const bookmarks =
-      JSON.parse(window.localStorage.getItem("bookmarks")) || [];
+      JSON.parse(window.localStorage.getItem("bookmarks") || "[]") || [];
     bookmarks.push(bookmark);
     window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     this.setBookmarksUi(bookmarks);
   }
   editBookmark(bookmark: BookmarkInterface, index: number) {
-    const bookmarks = JSON.parse(window.localStorage.getItem("bookmarks"));
+    const bookmarks = JSON.parse(
+      window.localStorage.getItem("bookmarks") || "[]"
+    );
     bookmarks[index] = bookmark;
     window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     this.setBookmarksUi(bookmarks);
   }
   getBookmarks() {
-    return JSON.parse(window.localStorage.getItem("bookmarks")) || [];
+    return JSON.parse(window.localStorage.getItem("bookmarks") || "[]") || [];
   }
   deleteBookmark(index: number) {
-    let bookmarks = JSON.parse(window.localStorage.getItem("bookmarks"));
+    let bookmarks = JSON.parse(
+      window.localStorage.getItem("bookmarks") || "[]"
+    );
     bookmarks = bookmarks.filter((bookmark, i) => i != index);
     window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     this.setBookmarksUi(bookmarks);
