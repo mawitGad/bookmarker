3494ac8deefba3c9e9f9cfe2278736bb653a6383
Mawit Gad <mawitgaddev@gmail.com>
2023-03-19 13:50:42 +0000

    Fix bookmarkStore import in ./types.ts
    The bookmarkStore file does not export
    BookmarkStore as default I had to put
    braces around it consequently.

--- types.ts
+++ types.ts
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
-import BookmarkStore from "./bookmarkStore";
+import { BookmarkStore } from "./bookmarkStore";
 
 export interface BookmarkProps {
   url: string;
