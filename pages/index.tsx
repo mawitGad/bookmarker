05dbbd1843e7fffa410ab29eefef2918d9bc2ac4
mawit <mawitgaddev@gmail.com>
2023-03-20 20:17:01 +0100

    Fix: make image fill whole screen including overflows in mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,29 +7,29 @@ import BookmarkEditor from "../components/bookmarkEditor/bookmarkEditor";
 import AddButton from "../components/addButton/addButton";
 
 export default function Home() {
-  const { bookmarks, adder, editor } = useIndex();
-  return (
-    <>
-      <Image
-        alt="background"
-        src={"/background.jpg"}
-        fill
-        className="object-cover"
-      />
-      <main className="w-screen h-screen p-8 text-white relative grid grid-rows-[50px_auto] ">
-        <span className="text-[32px] ">Bookmarker</span>
-        <section className="flex flex-wrap justify-center items-center overflow-scroll">
-          <div className="w-fit max-w-full flex flex-wrap cursor-pointer ">
-            {bookmarks.map((bookmark, index) => (
-              <Bookmark key={bookmark.url} {...bookmark} index={index} />
-            ))}
-            <AddButton />
-          </div>
-        </section>
-      </main>
+    const { bookmarks, adder, editor } = useIndex();
+    return (
+        <>
+            <main className="w-screen h-screen p-8 text-white relative grid grid-rows-[50px_auto] ">
+                <Image
+                    alt="background"
+                    src={"/background.jpg"}
+                    fill
+                    className="object-cover z-[-1]"
+                />
+                <span className="text-[32px] ">Bookmarker</span>
+                <section className="flex flex-wrap justify-center items-center overflow-scroll">
+                    <div className="w-fit max-w-full flex flex-wrap cursor-pointer ">
+                        {bookmarks.map((bookmark, index) => (
+                            <Bookmark key={bookmark.url} {...bookmark} index={index} />
+                        ))}
+                        <AddButton />
+                    </div>
+                </section>
+            </main>
 
-      {adder && <BookmarkAdder />}
-      {editor && <BookmarkEditor />}
-    </>
-  );
+            {adder && <BookmarkAdder />}
+            {editor && <BookmarkEditor />}
+        </>
+    );
 }
